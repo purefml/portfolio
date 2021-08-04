7342f61b4c0f796915920cf1242c42317bba0b91
Ruben James Dela Cruz <delacruzrubenjames@gmail.com>
2021-08-04 16:47:57 +0800

    Update index.js
    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Ruben from './Ruben';
-/* Custom CSS */
 import './assets/css/style.css';
 
 ReactDOM.render(
